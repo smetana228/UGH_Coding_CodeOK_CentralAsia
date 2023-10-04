0703e7486081351e965a9be0b6e08f9e8e975175
smetana228 <65881878+smetana228@users.noreply.github.com>
2023-10-04 22:16:25 +0600

    Add files via upload

---
+++ services/promo_code.py
@@ -0,0 +1,19 @@
+import random
+import string
+import datetime
+def daily_promo_code():
+    #gets the current date in the format YYYY-MM-DD
+    current_date = datetime.date.today().isoformat()
+
+    #seeds the random number generator with the current date
+    seed = int(current_date.replace("-", ""))
+    random.seed(seed)
+
+    #generates a random promo code with a specific format (XXX-XXXX-XXXX)
+    promo_code = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(3))
+    promo_code += '-'
+    promo_code += ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(4))
+    promo_code += '-'
+    promo_code += ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(4))
+    print(promo_code)
+daily_promo_code()
